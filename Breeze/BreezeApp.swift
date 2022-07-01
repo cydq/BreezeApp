efb014e50ff968a66e2f65fbdb5a31c6aef6b135
Brian Shao <dev@jzshao.com>
2022-07-01 14:36:22 -0700

    Initial Commit

---
+++ Breeze/BreezeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreezeApp.swift
+//  Breeze
+//
+//  Created by Brian Shao on 2022-07-01.
+//
+
+import SwiftUI
+
+@main
+struct BreezeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Breeze/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Breeze
+//
+//  Created by Brian Shao on 2022-07-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
